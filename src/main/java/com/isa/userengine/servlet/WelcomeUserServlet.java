c5ff5924c5bd7624ea99dc647d6c7e13591adfa7
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-10-13 09:02:05 +0200

    first request.getParameter used (servlet welcome-user

---
+++ src/main/java/com/isa/userengine/servlet/WelcomeUserServlet.java
@@ -0,0 +1,22 @@
+package com.isa.userengine.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/welcome-user")
+public class WelcomeUserServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+
+//        response.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<!DOCTYPE html><html><body><h2>Hello "+name+"</h2></body></html>");
+    }
+}
